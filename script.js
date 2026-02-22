610a3bde8473951644c2c7bac63d419d3c5011bd
Festival Poster <festivalposterfp@gmail.com>
2026-02-22 06:04:06 -0800

    Enhance loader and implement navbar scroll effect
    Refactor loader functionality and add navbar scroll effect.

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
-window.onload = function () {
+console.log("Website Loaded");
+
+/* LOADER */
+window.addEventListener("load", function(){
 
 const loader = document.getElementById("loader");
 
 if(loader){
-loader.style.display = "none";
+loader.classList.add("hide");
+
+setTimeout(()=>{
+loader.style.display="none";
+},600);
 }
 
-};
+});
+
+/* NAVBAR SCROLL EFFECT */
+window.addEventListener("scroll", function(){
+
+const nav=document.querySelector(".navbar");
+
+if(!nav) return;
+
+if(window.scrollY>50){
+nav.style.background="rgba(0,0,0,0.8)";
+}else{
+nav.style.background="rgba(255,255,255,0.05)";
+}
+
+});
