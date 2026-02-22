021da26e922ba9e0305f0acaee6be6504a42f8a7
Festival Poster <festivalposterfp@gmail.com>
2026-02-22 05:53:54 -0800

    Refactor loader display logic on window load

--- script.js
+++ script.js
@@ -1,44 +1,9 @@
-console.log("Website Loaded");
+window.onload = function () {
 
-/* POPUP */
-function openPopup(src){
-const popup=document.getElementById("popup");
-const img=document.getElementById("popupImg");
-
-if(popup && img){
-popup.style.display="flex";
-img.src=src;
-}
-}
-
-function closePopup(){
-const popup=document.getElementById("popup");
-
-if(popup){
-popup.style.display="none";
-}
-}
-
-/* LOADER */
-window.addEventListener("load", function(){
-const loader=document.getElementById("loader");
+const loader = document.getElementById("loader");
 
 if(loader){
-loader.style.display="none";
-}
-});
-
-/* NAVBAR SCROLL EFFECT */
-window.addEventListener("scroll", function(){
-
-const nav=document.querySelector(".navbar");
-
-if(!nav) return;
-
-if(window.scrollY>50){
-nav.style.background="rgba(0,0,0,0.8)";
-}else{
-nav.style.background="rgba(255,255,255,0.05)";
+loader.style.display = "none";
 }
 
-});
+};
